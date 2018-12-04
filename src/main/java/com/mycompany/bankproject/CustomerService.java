def3d36a360b10de9a3e57bf8c1491c05a2b0d8c
suren <suren@192.168.224.70>
2018-12-04 12:59:51 +0000

    gfcvj

--- src/main/java/com/mycompany/bankproject/CustomerService.java
+++ src/main/java/com/mycompany/bankproject/CustomerService.java
@@ -35,14 +35,13 @@ import javax.ws.rs.core.UriInfo;
 
 @Path("/customers")
 public class CustomerService {
-    
+
     EntityManager entityManager;
-    
-    public CustomerService(){
+
+    public CustomerService() {
         EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("test-connection");
         entityManager = emfactory.createEntityManager();
     }
-    
 
 //    @GET
 //    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
@@ -54,29 +53,29 @@ public class CustomerService {
 //    
 //    }
 //    
-    
     @GET
     @Path("bank")
     public Response get() {
-    CacheControl cc = new CacheControl();
-    cc.setMaxAge(10000);
-    System.out.println("\n\n\n\n+go");
-    return Response.ok("Some Data").cacheControl(cc).build();
-    
-}
+        CacheControl cc = new CacheControl();
+        cc.setMaxAge(10000);
+        System.out.println("\n\n\n\n+go");
+        return Response.ok("Some Data").cacheControl(cc).build();
+
+    }
 
     @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public Response getCustomers() {
-  
+
         List<BankCustomer> list = allEntries();
 
-        GenericEntity entity = new GenericEntity<List<BankCustomer>>(list){};
+        GenericEntity entity = new GenericEntity<List<BankCustomer>>(list) {
+        };
         return Response.ok(entity).build();
-    
+
     }
-    
-     public List<BankCustomer> allEntries() {
+
+    public List<BankCustomer> allEntries() {
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<BankCustomer> cq = cb.createQuery(BankCustomer.class);
         Root<BankCustomer> rootEntry = cq.from(BankCustomer.class);
@@ -84,7 +83,7 @@ public class CustomerService {
         TypedQuery<BankCustomer> allQuery = entityManager.createQuery(all);
         return allQuery.getResultList();
     }
-     
+
 //    @GET
 //    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 //    @Path("{id}")
@@ -92,45 +91,60 @@ public class CustomerService {
 //        Planet test = entityManager.find(Planet.class, id);
 //        return test;
 //    }
-     
     @GET
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     @Path("{customer_id}")
     public BankCustomer getCustomer(@PathParam("customer_id") int customer_id) {
         BankCustomer test = entityManager.find(BankCustomer.class, customer_id);
-        if (test == null){
+        if (test == null) {
             throw new NotFoundException("You dun goofed");
         }
         return test;
     }
-     
-    
-//    @GET
-//    @Path("/save")
-//    @Produces("application/json")
-//    public Response save(@Context UriInfo info){
-//        Gson gson = new Gson();
-//        
-//        String first_name = info.getQueryParameters().getFirst("first_name");
-//        String last_name = info.getQueryParameters().getFirst("last_name");
-//        String address = info.getQueryParameters().getFirst("address");
-//        String phone_number = info.getQueryParameters().getFirst("phone_number");
-//        String email = info.getQueryParameters().getFirst("email");
-//        String inputted_password = info.getQueryParameters().getFirst("inputted_password");
-//        String encrypted_password = info.getQueryParameters().getFirst("encrypted_password");
-//        boolean verification = Boolean.parseBoolean(info.getQueryParameters().getFirst("verification"));
+
+    @POST
+    @Path("/save")
+   //@Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response save(@Context UriInfo info) {
+        BankCustomer bc = new BankCustomer();
+
+        String first_name = info.getQueryParameters().getFirst("first_name");
+        String last_name = info.getQueryParameters().getFirst("last_name");
+        String address = info.getQueryParameters().getFirst("address");
+        String phone_number = info.getQueryParameters().getFirst("phone_number");
+        String email = info.getQueryParameters().getFirst("email");
+        String inputted_password = info.getQueryParameters().getFirst("inputted_password");
+        // String encrypted_password = info.getQueryParameters().getFirst("encrypted_password");
+        // boolean verification = Boolean.parseBoolean(info.getQueryParameters().getFirst("verification"));
 //        int pin = Integer.parseInt(info.getQueryParameters().getFirst("pin"));
 //        String status = "ok";
-//        
-//        
-//
-//        BankCustomer bc = new BankCustomer();
-//        System.out.println("");
-//
-//       return Response.status(200).entity(gson.toJson(bc)).build();
-//       
-//    }
-    
+
+       
+        bc.setFirst_name(first_name);
+        bc.setLast_name(last_name);
+        bc.setAddress(address);
+        bc.setPhone_number(phone_number);
+        bc.setEmail(email);
+        bc.setInputted_password(inputted_password);
+        bc.getFirst_name();
+        bc.getLast_name();
+        bc.getEmail();
+        bc.getAddress();
+        bc.getPhone_number();
+        bc.getInputted_password();
+
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(bc);
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        
+        return Response.status(200).entity(bc).build();
+
+    }
+/*
     @POST
     @Path("/save")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -141,10 +155,11 @@ public class CustomerService {
 
         entityManager.persist(c);
         entityManager.getTransaction().commit();
-        
+
         entityManager.close();
         entityManager.close();
 
         return Response.status(200).entity(c).build();
     }
+*/
 }
